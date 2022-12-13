ede3d0de74bf57d90ed3b808b9e70c58f3f7dc68
Ryan Sullivan <mrsulliv@gmail.com>
2022-12-13 12:30:09 -0800

    style 404 page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout() {
   return (
     <>
       <nav className="flex justify-between items-center px-10 py-5 shadow-md">
-        <Link to="items">Shop By Type</Link>
+        <Link to="shop">Shop</Link>
         <Link to="/" className="font-bold text-3xl">Mos Espa Marketplace</Link>
         <div className='flex items-center'>
           <UserIcon className='w-12 h-12 mr-4'/>
