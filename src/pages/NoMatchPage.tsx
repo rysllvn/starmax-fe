03bf740427cd88008e5230b814c20656cdb5cde5
Ryan Sullivan <mrsulliv@gmail.com>
2022-12-14 12:50:43 -0800

    update 404 page

--- src/pages/NoMatchPage.tsx
+++ src/pages/NoMatchPage.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
+import './NoMatchPage.css';
+
 export default function NoMatchPage() {
   const navigate = useNavigate();
 
@@ -7,9 +9,9 @@ export default function NoMatchPage() {
     navigate(-1);
   };
   return (
-    <div className='flex flex-col items-center'>
-      <h1 className='text-3xl mb-3'>Page Not Found</h1>
-      <button className='bg-slate-100 p-4 rounded' onClick={handleBack}>Take me back</button>
+    <div id="no-match-container" className='h-full flex flex-col items-center p-48'>
+      <h1 className='bg-slate-100 p-8 text-3xl mb-3 rounded'>404: These aren't the droids you're looking for (page not found)</h1>
+      <button className='bg-emerald-300 p-4 rounded text-3xl' onClick={handleBack}>Take me back</button>
     </div>
   )
 }
