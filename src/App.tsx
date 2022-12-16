62a9333c35e85b9884ebc1ecf9b53326bce63f34
Ryan Sullivan <mrsulliv@gmail.com>
2022-12-16 13:53:04 -0800

    check localstorage for user on login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useReducer } from 'react';
 // https://github.com/remix-run/react-router/tree/dev/examples
 
 import './App.css';
 import { AuthContext, CartContext, DispatchContext } from './utilities/Contexts';
 import { initialState, reducer } from './utilities/AppReducer';
 import Router from './components/Router';
-import { SET_USER_ACTION_TYPE, USER_DATA_KEY } from './utilities/constants';
 
 function App() {
   /***************************************************************
@@ -13,13 +12,6 @@ function App() {
   //  see AppReducer for information
   ****************************************************************/
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    const userData = localStorage.getItem(USER_DATA_KEY);
-    if (userData) {
-      dispatch({ type: SET_USER_ACTION_TYPE, userData });
-    }
-  }, [dispatch]);
 
   return (
     <DispatchContext.Provider value={dispatch}>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
-import { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import eCommerce_API from "../utilities/ApiConfig";
 import { SET_USER_ACTION_TYPE, USER_DATA_KEY } from '../utilities/constants';
 import { DispatchContext } from "../utilities/Contexts";
@@ -10,6 +10,17 @@ export default function LoginPage() {
     const [error,setError] = useState<string>("");
     const navigate = useNavigate();
     const dispatch = useContext(DispatchContext);
+    const location = useLocation();
+
+    const from = location.state?.from.pathname || '/';
+
+    useEffect(() => {
+      const userData = localStorage.getItem(USER_DATA_KEY);
+      if (userData) {
+        dispatch({ type: SET_USER_ACTION_TYPE, userData: JSON.parse(userData) });
+        navigate(from, { replace: true });
+      }
+    }, [dispatch, navigate, from]);
 
     function handleEmail(event:React.ChangeEvent<HTMLInputElement>) {
         setEmail(event.target.value);
@@ -28,14 +39,13 @@ export default function LoginPage() {
             const userData = resp.data;
             localStorage.setItem(USER_DATA_KEY, JSON.stringify(userData));
             dispatch({type: SET_USER_ACTION_TYPE, userData });
-            navigate("/");
+            navigate(from, { replace: true })
         })
-          .catch((e) => setError(e.response.data.message));
+        .catch((e) => setError(e.response.data.message));
     }
 
     return(
-
-        <>
+      <>
         <h1 className="flex flex-col items-center text-3xl font-bold">Login Page</h1>
         <div className="mx-60">
             <form className="bg-emerald-300 flex flex-col items-center gap-5 rounded-lg mx-80 my-8 py-5 px-10" onSubmit={handleSubmit}>
@@ -47,7 +57,7 @@ export default function LoginPage() {
                <button className="bg-slate-800 rounded-md text-white mt-2 px-5 py-2 ease-out duration-300 hover:scale-125">LOGIN</button>
                <Link to={"/signup"} className="text-blue-700 underline">Create new account</Link>
             </form>
-        </div>
-            </>
+          </div>
+        </>
     )
 }
